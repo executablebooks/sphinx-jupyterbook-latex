96d214be169c0711c5529fbe8157ee4d4c307a99
AakashGC <aakashgfude@gmail.com>
2020-11-06 10:13:52 +1100

    added a cls file

--- jupyterbook_latex/__init__.py
+++ jupyterbook_latex/__init__.py
@@ -1,14 +1,28 @@
 from .nodes import HiddenCellNode, visit_HiddenCellNode
 from .transforms import codeCellTransforms
 from sphinx import builders
+from sphinx.util.fileutil import copy_asset_file
+from pathlib import Path
 
 __version__ = "0.1.0"
 """jupyterbook-latex version"""
 
 
-def post_transform_handler(app):
+def build_init_handler(app):
+    # only allow latex builder to access rest of the features
     if isinstance(app.builder, builders.latex.LaTeXBuilder):
         app.add_post_transform(codeCellTransforms)
+        copy_static_files(app)
+
+
+def add_necessary_config(app, config):
+    config["latex_theme"] = "jupyterBook"
+
+
+def copy_static_files(app):
+    themePath = Path(__file__).parent.joinpath("theme")
+    clsFile = themePath.joinpath("jupyterBook.cls")
+    copy_asset_file(str(clsFile), app.outdir)
 
 
 def setup(app):
@@ -21,4 +35,5 @@ def setup(app):
         text=(visit_HiddenCellNode, None),
         man=(visit_HiddenCellNode, None),
     )
-    app.connect("builder-inited", post_transform_handler)
+    app.connect("config-inited", add_necessary_config)
+    app.connect("builder-inited", build_init_handler)
