27b02982251574b188f31b2bf3b5baa717b1de4d
AakashGC <aakashgfude@gmail.com>
2020-12-10 20:53:05 +1100

    pytest mark

--- tests/test_toc.py
+++ tests/test_toc.py
@@ -1,6 +1,8 @@
+import pytest
 from jupyter_book import commands
 
 
+@pytest.mark.requires_tex
 def test_toc(cli, file_regression, rootdir):
     path_partsToc = rootdir.joinpath("test-partsToc")
     cmd = f"{path_partsToc} --builder pdflatex"
