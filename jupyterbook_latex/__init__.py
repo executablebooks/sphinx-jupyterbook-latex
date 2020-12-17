484fb43b9ee7c776c5a3f4576c6d4f4c122a34c2
AakashGC <aakashguptachoudhury@gmail.com>
2020-12-17 18:55:33 +1100

    🚀 RELEASE: v0.1.1 (#18)
    * increased version
    
    * edited changelog

--- jupyterbook_latex/__init__.py
+++ jupyterbook_latex/__init__.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 from docutils import nodes as docnodes
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 """jupyterbook-latex version"""
 
 logger = logging.getLogger(__name__)
