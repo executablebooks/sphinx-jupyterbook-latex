dbbcd21a8ccc8340298fc5f10f5821d81db425d4
AakashGC <aakashguptachoudhury@gmail.com>
2020-12-20 00:40:20 +1100

    release file changes (#22)

--- jupyterbook_latex/__init__.py
+++ jupyterbook_latex/__init__.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 from docutils import nodes as docnodes
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 """jupyterbook-latex version"""
 
 logger = logging.getLogger(__name__)
