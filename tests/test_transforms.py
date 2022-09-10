04a894983e675495fe0ca4a41f5ede638892dc0b
AakashGC <aakashguptachoudhury@gmail.com>
2022-09-11 05:49:58 +1000

    trying out with bs4

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,10 +1,28 @@
 import pytest
+from bs4 import BeautifulSoup
+from myst_nb import __version__ as mystnb_version
 
 
 @pytest.mark.sphinx("latex", testroot="MystNbPostTransform")
 def test_hide_input(app, get_sphinx_app_doctree):
     app.build()
-    get_sphinx_app_doctree(app, docname="cell-hide-input", resolve=True, regress=True)
+    _, minor = mystnb_version.split(".")[0:2]
+    if int(minor) < 14:
+        get_sphinx_app_doctree(
+            app, docname="cell-hide-input", resolve=True, regress=True
+        )
+    else:
+        doctree = get_sphinx_app_doctree(
+            app, docname="cell-hide-input", resolve=True, regress=False
+        )
+        xml = BeautifulSoup(str(doctree), "html.parser")
+        try:
+            hiddencellnode = xml.find_all("container", class_="tag_hide-input").find(
+                "hiddencellnode"
+            )
+            assert "cell_input" in hiddencellnode["classes"]
+        except NameError:
+            return False
 
 
 @pytest.mark.sphinx("latex", testroot="MystNbPostTransform")
