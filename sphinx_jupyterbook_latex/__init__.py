709eeb4883e5fafb648d08147124d6342a7fe275
AakashGC <aakashguptachoudhury@gmail.com>
2022-11-16 11:06:59 +1100

    sjl version

--- sphinx_jupyterbook_latex/__init__.py
+++ sphinx_jupyterbook_latex/__init__.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 """sphinx-jupyterbook-latex version"""
 
 
